6988c7d4ebedc0b51382a04e394216f4206a95b2
zotroneneis <popkes@gmx.net>
2018-05-25 17:04:05 -0700

    first problem solved

---
+++ problem001.py
@@ -0,0 +1,16 @@
+# Find the sum of all the multiples of 3 or 5 below 1000.
+# https://projecteuler.net/problem=1
+
+# Simple version with list comprehension
+def find_sum_of_multiples(n):
+    multiples = [num for num in range(n) if num % 3 == 0 or num % 5 == 0]
+    return sum(multiples)
+
+print(find_sum_of_multiples(1000))
+
+# Faster version with generators
+def find_sum_of_multiples(n):
+    sum_of_multiples = sum( num for num in range(n) if num % 3 == 0 or num % 5 == 0 )
+    return sum_of_multiples
+
+print(find_sum_of_multiples(1000))
